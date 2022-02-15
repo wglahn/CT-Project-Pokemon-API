e27af214262dcd4452688f6a02d033707cd538aa
wglahn <wglahn@yahoo.com>
2022-02-15 12:35:53 -0600

    wtforms

--- app/blueprints/auth/forms.py
+++ app/blueprints/auth/forms.py
@@ -2,7 +2,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import Email, DataRequired, EqualTo, ValidationError
-from app.models import User
 
 class LoginForm(FlaskForm):
     #field name = DatatypeField('LABEL', validators=[LIST OF validators])
